dbc8d7107769801948028ad0fb81e5b3d6611f81
Chandan <hichandu810chandupathak@gmail.com>
2026-09-03 15:23:47 +0530

    Add Android VoiceInteractionService entry point

---
+++ app/src/main/java/com/bixby/voiceassistant/BixbyVoiceInteractionService.kt
@@ -0,0 +1,10 @@
+package com.bixby.voiceassistant
+
+import android.service.voice.VoiceInteractionService
+
+/** Android Assistant entry point for Bixby. */
+class BixbyVoiceInteractionService : VoiceInteractionService() {
+    override fun onReady() {
+        super.onReady()
+    }
+}
