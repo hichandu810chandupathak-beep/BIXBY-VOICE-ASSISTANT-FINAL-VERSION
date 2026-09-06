4e2a1935306009c16ca74898652d1d34eb1c4d61
Chandan <hichandu810chandupathak@gmail.com>
2026-09-06 11:12:45 +0530

    Nuke startup initialization from Application and simplify Welcome startup

--- app/src/main/java/com/bixby/voiceassistant/BixbyApplication.kt
+++ app/src/main/java/com/bixby/voiceassistant/BixbyApplication.kt
@@ -1,24 +1,9 @@
 package com.bixby.voiceassistant
 
 import android.app.Application
-import android.os.Handler
-import android.os.Looper
-import android.util.Log
-import android.widget.Toast
 
 class BixbyApplication : Application() {
     override fun onCreate() {
         super.onCreate()
-        Thread.setDefaultUncaughtExceptionHandler { thread, throwable ->
-            try {
-                val message = throwable.message ?: throwable.javaClass.simpleName
-                Log.e("BixbyCrash", "Fatal crash on ${thread.name}: $message", throwable)
-                Handler(Looper.getMainLooper()).post {
-                    Toast.makeText(this, "Bixby crash: $message", Toast.LENGTH_LONG).show()
-                }
-            } catch (handlerError: Exception) {
-                Log.e("BixbyCrash", "Crash handler failed", handlerError)
-            }
-        }
     }
 }
